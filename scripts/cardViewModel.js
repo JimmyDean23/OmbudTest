75f3859e077b83bc0e7ffe33d1fceab59f4eb34a
Billy Dixon <bdixon2340@gmail.com>
2014-11-19 19:14:09 -0600

    Added ruby backend

--- scripts/cardViewModel.js
+++ scripts/cardViewModel.js
@@ -1,5 +1,11 @@
 function cardViewModel() {
-	this.firstName = ko.observable("Bob");	
+	this.firstName = ko.observable("Bob");
+	this.lastName = ko.observable("Smith");
+	this.emailAddress = ko.observable("example@gmail.com");
+	this.phoneNumber = ko.observable("555-1234");
+	this.photo = ko.observable("");
+	this.headline = ko.observable("Headline");
+	this.currentPosition = ko.observable("Engineer");
 }
 
 ko.applyBindings(new cardViewModel());
